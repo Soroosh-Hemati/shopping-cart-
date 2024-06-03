442191fb292ee040cf716bbf1d1b39b6243070d7
Soroosh-Hemati <sorooshhemati4@gmail.com>
2024-06-03 15:51:10 +0330

    Add HTML structure and CSS and design needed classes

---
+++ public/app.js
@@ -0,0 +1,40 @@
+import { productsData } from "./products.js";
+const productsWrapper = document.querySelector(".products-wrapper");
+
+class Products {
+  getProducts() {
+    return productsData;
+  }
+}
+class UI {
+  showProducts(products) {
+    let resault = "";
+    products.forEach((item) => {
+      resault += `<div class="product">
+            <img
+              src="${item.imageURL}"
+              alt=""
+              class="product-img w-full max-w-[300px] h-full max-h-[200px] object-cover"
+            />
+            <div class="product-info flex flex-col justify-center items-center py-5">
+              <div class="product-name uppercase mb-2 text-violet-700">${item.title}</div>
+              <div class="product-price mb-2 text-violet-700 font-semibold">
+                $${item.price}
+              </div>
+              <button
+                class="add-to-cart bg-violet-100 text-violet-700 px-2 py-1 rounded"
+              >
+                <i class="fa fa-shopping-cart mr-2"></i>Add to Cart
+              </button>
+            </div>
+          </div>`;
+    });
+    productsWrapper.innerHTML = resault;
+  }
+}
+class Storage {}
+document.addEventListener("DOMContentLoaded", () => {
+  const products = new Products();
+  const ui = new UI();
+  ui.showProducts(products.getProducts());
+});
---
+++ public/products.js
@@ -0,0 +1,57 @@
+export const productsData = [
+  {
+    id: 1,
+    title: "Baked Chicken",
+    price: 25.0,
+    imageURL: "./images/baked-chicken-wings-with-teriyaki-sauce.jpg",
+  },
+  {
+    id: 2,
+    title: "Chicken Skewers",
+    price: 30.0,
+    imageURL: "./images/chicken-skewers-with-slices-sweet-peppers-dill.jpg",
+  },
+  {
+    id: 3,
+    title: "Pasta",
+    price: 50.0,
+    imageURL: "./images/pic2.jpg",
+  },
+  {
+    id: 4,
+    title: "Pepperoni Pizza",
+    price: 35.0,
+    imageURL: "./images/pic6.jpg",
+  },
+  {
+    id: 5,
+    title: "Soup",
+    price: 28.0,
+    imageURL: "./images/pic4.jpg",
+  },
+  {
+    id: 6,
+    title: "Hamburger",
+    price: 44.0,
+    imageURL: "./images/pexels-robinstickel-70497.jpg",
+  },
+  {
+    id: 7,
+    title: "Penne Pasta",
+    price: 61.0,
+    imageURL:
+      "./images/penne-pasta-tomato-sauce-with-chicken-tomatoes-wooden-table.jpg",
+  },
+  {
+    id: 8,
+    title: "American Pizza",
+    price: 43.0,
+    imageURL: "./images/pic6.jpg",
+  },
+  {
+    id: 9,
+    title: "Milkshake",
+    price: 15.0,
+    imageURL: "./images/pic5.jpg",
+  },
+];
